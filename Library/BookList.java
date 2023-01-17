e0b7e1c17b923cbbd0bfa74b249cfa1626d398f0
destevae <elainetang214@gmail.com>
2023-01-17 09:42:10 -0500

    library project

--- Library/BookList.java
+++ Library/BookList.java
@@ -22,22 +22,32 @@ public class BookList {
         
         // https://techvidvan.com/tutorials/read-csv-file-in-java/
         Scanner sc = new Scanner(new File("./booklist.csv"));
-        // setting comma as delimiter pattern
-        sc.useDelimiter(",");
         
         while(sc.hasNext()){ // creates new book, calls addToList method
             if(genre.equals("Nonfiction")){
-                NonfictionBook mybook = new NonfictionBook(sc.next());
+                NonfictionBook mybook = new NonfictionBook(sc.nextLine());
+
+                // set the csv line's content to the nonfiction book's attributes
+
+
+
+                // if item is already in arraylist, then add to quantity
+                if(NonfictionBook.integers > 0){
+                    int x = (int) NonfictionBook.integers;
+                    x++;
+                    NonfictionBook.integers = x;
+                }
+                
                 booklist.add(mybook);
             }
 
             if(genre.equals("Fiction")){
-                FictionBook mybook = new FictionBook(sc.next());
+                FictionBook mybook = new FictionBook(sc.nextLine());
                 booklist.add(mybook);
             }
 
             if(genre.equals("TextBook")){
-                TextBook mybook = new TextBook(sc.next());
+                TextBook mybook = new TextBook(sc.nextLine());
                 booklist.add(mybook);
             }
         }
--- Library/NonfictionBook.java
+++ Library/NonfictionBook.java
@@ -1,6 +1,7 @@
 package Library;
 
 public class NonfictionBook extends Book{
+    static int integers;
     
     NonfictionBook(String csvLine){
         // break it up after using split and assign to variables
